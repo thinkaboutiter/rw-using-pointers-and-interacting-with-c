d5c42e0691edb0393d506c036e9baa4f6c2e49a4
Boyan Yankov <boyan.yankov@igsoft.com>
2018-01-15 12:10:22 +0200

    added compression logic

--- Compression.playground/Contents.swift
+++ Compression.playground/Contents.swift
@@ -18,8 +18,94 @@ enum CompressionOperation {
 func perform(_ operation: CompressionOperation,
              on input: Data,
              using algorithm: CompressionAlgorithm,
-             workingBufferSize: Int = 2000) -> Data?  {
-    return nil
+             workingBufferSize: Int = 2000) -> Data?
+{
+    // set the algorithm
+    let streamAlgorithm: compression_algorithm
+    switch algorithm {
+    case .lz4:
+        streamAlgorithm = COMPRESSION_LZ4
+    case .lz4a:
+        streamAlgorithm = COMPRESSION_LZMA
+    case .zlib:
+        streamAlgorithm = COMPRESSION_ZLIB
+    case .lzfse:
+        streamAlgorithm = COMPRESSION_LZFSE
+    }
+    
+    // set the stream operation and flags
+    let streamOperation: compression_stream_operation
+    let flags: Int32
+    switch operation {
+    case .compression:
+        streamOperation = COMPRESSION_STREAM_ENCODE
+        flags = Int32(COMPRESSION_STREAM_FINALIZE.rawValue)
+    case .decompression:
+        streamOperation = COMPRESSION_STREAM_DECODE
+        flags = 0
+    }
+    
+    // 1: create a stream
+    var streamPointer = UnsafeMutablePointer<compression_stream>.allocate(capacity: 1)
+    defer {
+        streamPointer.deallocate(capacity: 1)
+    }
+    
+    // 2: initialize the stream
+    var stream = streamPointer.pointee
+    var status = compression_stream_init(&stream, streamOperation, streamAlgorithm)
+    guard status != COMPRESSION_STATUS_ERROR else {
+        return nil
+    }
+    defer {
+        compression_stream_destroy(&stream)
+    }
+    
+    // 3: set up a destination buffer
+    let dstSize = workingBufferSize
+    let dstPointer = UnsafeMutablePointer<UInt8>.allocate(capacity: dstSize)
+    defer {
+        dstPointer.deallocate(capacity: dstSize)
+    }
+    
+    // process the input
+    return input.withUnsafeBytes { (srcPointer: UnsafePointer<UInt8>) in
+        // 1
+        var output = Data()
+        
+        // 2
+        stream.src_ptr = srcPointer
+        stream.src_size = input.count
+        stream.dst_ptr = dstPointer
+        stream.dst_size = dstSize
+        
+        // 3
+        while status == COMPRESSION_STATUS_OK {
+            // process the stream
+            status = compression_stream_process(&stream, flags)
+            
+            // collect bytes from the stream and reset
+            switch status {
+                
+            case COMPRESSION_STATUS_OK:
+                // 4
+                output.append(dstPointer, count: dstSize)
+                stream.dst_ptr = dstPointer
+                stream.dst_size = dstSize
+                
+            case COMPRESSION_STATUS_ERROR:
+                return nil
+                
+            case COMPRESSION_STATUS_END:
+                // 5
+                output.append(dstPointer, count: stream.dst_ptr - dstPointer)
+                
+            default:
+                fatalError()
+            }
+        }
+        return output
+    }
 }
 
 // Compressed keeps the compressed data and the algorithm
